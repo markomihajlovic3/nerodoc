7726c0d34bdf4ee712aa09724a4af9b1444017c0
Marko Mihajlovic <markomihajlovic3@gmail.com>
2016-04-28 20:15:55 +0200

    Update conf.php

--- src/config/conf.php
+++ src/config/conf.php
@@ -12,8 +12,8 @@ return [
     //database config
     'db_hostname' => 'localhost',
     'db_username' => 'root',
-    'db_password' => 'street27',
-    'db_name'     => 'larablog',
+    'db_password' => 'password',
+    'db_name'     => 'database',
 
     //default route config(used with Basic Router)
     'default_controller' => 'Welcome',
